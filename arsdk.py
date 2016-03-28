2e4e788fc3d9284202177089ea9c10600cb167be
b3b <ash.b3b@gmail.com>
2016-03-28 05:04:52 +0300

    add ARSDK network packet packer/unpacker

--- arsdk.py
+++ arsdk.py
@@ -1,3 +1,5 @@
+import struct
+
 data_types = {
     'ack': 1,
     'data': 2,
@@ -63,3 +65,53 @@ command_names = {(v['project_id'], vv['class_id'], vv['commands'].index(cmd)):cm
                  for cmd in vv['commands']}
 
 UPDATE_NOTIFICATION_DESCRIPTOR_UUID = "00002902-0000-1000-8000-00805f9b34fb"
+
+class Packet(object):
+    """Network packet packer/unpacker
+    Packet format:
+           |data type|sequence_number|project ID|command class ID|command ID|command arguments...|
+    Sizes:  1 byte    1byte           1 byte     1 byte           2 bytes    differ
+
+    >>> Packet.unpack([2, 1, 2, 0, 4, 0])
+    <Packet DT:2(data) SQ:1 PJ:2(mini_drone) CL:0(Piloting) CC:4(Emergency) args:[]>
+    >>> Packet.unpack([2, 3, 2, 1, 2, 0, 1])
+    <Packet DT:2(data) SQ:3 PJ:2(mini_drone) CL:1(SpeedSettings) CC:2(Wheels) args:[1]>
+    >>> Packet(data_type=2, sequence_number=3, project_id=0,
+    ... class_id=5, command_id=1, arguments=[95]).pack()
+    [2, 3, 0, 5, 1, 0, 95]
+    """
+
+    fmt = '<Packet DT:{DT}({dt}) SQ:{SQ} PJ:{PJ}({pj}) CL:{CL}({cl}) CC:{CC}({cc}) args:{args}>'
+
+    def __init__(self, data_type, sequence_number, project_id, class_id, command_id, arguments):
+        self.data_type = data_type
+        self.sequence_number = sequence_number
+        self.project_id = project_id
+        self.class_id = class_id
+        self.command_id = command_id
+        self.arguments = arguments or []
+
+    def __repr__(self):
+        cl = command_classes_names.get((self.project_id, self.class_id), '??')
+        cc = command_names.get((self.project_id, self.class_id, self.command_id), '??')
+        return self.fmt.format(
+            DT=self.data_type, dt=data_type_names.get(self.data_type, '??'),
+            SQ=self.sequence_number,
+            PJ=self.project_id, pj=project_names.get(self.project_id, '??'),
+            CL=self.class_id, cl=cl,
+            CC=self.command_id, cc=cc,
+            args=list(self.arguments)
+        )
+
+    def pack(self):
+        return  [self.data_type, self.sequence_number,
+                 self.project_id,self. class_id,
+                 self.command_id,
+                 0 # command ID second byte
+        ] + self.arguments
+
+    @classmethod
+    def unpack(self, data):
+        unpacked = struct.unpack_from('<BBBBH', buffer(bytearray(data)))
+        arguments = data[6:]
+        return Packet(*unpacked+(arguments,))
